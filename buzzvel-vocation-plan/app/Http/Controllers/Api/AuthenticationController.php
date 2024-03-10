e3eeac1c26b85fe605a31dff99b237cb5ac3c2fd
Francisco Samir <34774556+fscpinheiro@users.noreply.github.com>
2024-03-10 01:12:53 -0300

    Criação do User Seeder, Endpoint Login and Logout

---
+++ buzzvel-vocation-plan/app/Http/Controllers/Api/AuthenticationController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class AuthenticationController extends Controller
+{
+    public function store()
+    {
+        if (Auth::attempt(['email' => request('email'), 'password' => request('password')])) {
+            // OK Auth
+            $user = User::find(Auth::user()->id);
+            $user_token['token'] = $user->createToken('appToken')->accessToken;
+
+            return response()->json([
+                'success' => true,
+                'token' => $user_token,
+                'user' => $user,
+            ], 200);
+        } else {
+            // failure to authenticate
+            return response()->json([
+                'success' => false,
+                'message' => 'Failed to authenticate.',
+            ], 401);
+        }
+    }
+
+    public function destroy(Request $request)
+    {
+        if (Auth::user()) {
+            $request->user()->token()->revoke();
+            return response()->json([
+                'success' => true,
+                'message' => 'Logged out successfully',
+            ], 200);
+        }
+    }
+}
